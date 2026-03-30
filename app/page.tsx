dd171427b512b36a83cf15262e36df003d27f965
Amr ElToukhy <amr2553473@gmail.com>
2026-03-30 18:17:57 +0200

    test deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation"
 
 export default function RootPage() {
+  <h1>AMR TEST 🔥🔥🔥</h1>
   // تحويل الزائر تلقائياً إلى صفحة الرئيسية الجديدة
   redirect("/home")
 }
